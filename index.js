02f2e69b5a8bba502835822918117e2abdcd77e6
Zulqarnain Sikander <114936748+mahersiyal1122@users.noreply.github.com>
2023-12-23 17:20:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 //................Random Player 1..................
 var randomNumber1=(Math.floor(Math.random() * 6)) + 1;
 if (randomNumber1===1){
-    document.querySelector(".randomplayer1").setAttribute("src","images/dice1.png");
+    document.querySelector(".randomplayer1").setAttribute("src","dice1.png");
 }
 else if (randomNumber1===2){
-    document.querySelector(".randomplayer1").setAttribute("src","images/dice2.png");
+    document.querySelector(".randomplayer1").setAttribute("src","dice2.png");
 }
 else if (randomNumber1===3){
-    document.querySelector(".randomplayer1").setAttribute("src","images/dice3.png");
+    document.querySelector(".randomplayer1").setAttribute("src","dice3.png");
 }
 else if (randomNumber1===4){
-    document.querySelector(".randomplayer1").setAttribute("src","images/dice4.png");
+    document.querySelector(".randomplayer1").setAttribute("src","dice4.png");
 }
 else if (randomNumber1===5){
-    document.querySelector(".randomplayer1").setAttribute("src","images/dice5.png");
+    document.querySelector(".randomplayer1").setAttribute("src","dice5.png");
 }
 else {
-    document.querySelector(".randomplayer1").setAttribute("src","images/dice6.png");
+    document.querySelector(".randomplayer1").setAttribute("src","dice6.png");
 }
 
 //................Random Player 2..................
 var randomNumber2=(Math.floor(Math.random() * 6)) + 1;
 if (randomNumber2===1){
-    document.querySelector(".randomplayer2").setAttribute("src","images/dice1.png");
+    document.querySelector(".randomplayer2").setAttribute("src","dice1.png");
 }
 else if (randomNumber2===2){
-    document.querySelector(".randomplayer2").setAttribute("src","images/dice2.png");
+    document.querySelector(".randomplayer2").setAttribute("src","dice2.png");
 }
 else if (randomNumber2===3){
-    document.querySelector(".randomplayer2").setAttribute("src","images/dice3.png");
+    document.querySelector(".randomplayer2").setAttribute("src","dice3.png");
 }
 else if (randomNumber2===4){
-    document.querySelector(".randomplayer2").setAttribute("src","images/dice4.png");
+    document.querySelector(".randomplayer2").setAttribute("src","dice4.png");
 }
 else if (randomNumber2===5){
-    document.querySelector(".randomplayer2").setAttribute("src","images/dice5.png");
+    document.querySelector(".randomplayer2").setAttribute("src","dice5.png");
 }
 else {
-    document.querySelector(".randomplayer2").setAttribute("src","images/dice6.png");
+    document.querySelector(".randomplayer2").setAttribute("src","dice6.png");
 }
 
 //........ Who Will Win ........
